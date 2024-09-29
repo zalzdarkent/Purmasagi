64e8b50977a33220148caad52ceb4dc62ce45ffb
Zalz <arszalzdarkent@gmail.com>
2024-09-29 09:33:12 +0700

    first commit

---
+++ routes/app.js
@@ -0,0 +1,17 @@
+const nilai = 90
+
+if(nilai >= 90) {
+    console.log("A")
+}
+else if(nilai >= 80 && nilai <= 90) {
+    console.log("B")
+} 
+else if(nilai >= 70 && nilai <= 80) {
+    console.log("C");
+}
+else if(nilai >= 60 && nilai <= 70) {
+    console.log("D");
+}
+else {
+    console.log("E");
+}
