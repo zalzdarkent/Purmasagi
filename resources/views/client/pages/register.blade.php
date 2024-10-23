01bd1913662f10840624df725e44e30557fca193
Arina <aqmarinasha@gmail.com>
2024-10-23 23:07:29 +0700

    feat: change input kelas to select

--- resources/views/client/pages/register.blade.php
+++ resources/views/client/pages/register.blade.php
@@ -50,8 +50,13 @@
                                 <label for="kelas"
                                     class="block text-sm font-medium leading-6 text-gray-900">Kelas</label>
                                 <div class="mt-2">
-                                    <input id="kelas" name="kelas" type="text" autocomplete="email" required
-                                        class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    <select id="kelas" name="kelas" required
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        <option value="" disabled selected>Pilih kelas</option>
+                                        <option value="10">Kelas 10</option>
+                                        <option value="11">Kelas 11</option>
+                                        <option value="12">Kelas 12</option>
+                                    </select>
                                     <span id="kelasError" class="text-sm text-red-500"></span>
                                 </div>
                             </div>
